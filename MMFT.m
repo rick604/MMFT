36a4b5eb78b3114864c0167c8f6c8784a5dde425
YinHao Cai <72913410+rick604@users.noreply.github.com>
2021-11-25 10:35:11 +0800

    Update MMFT.m

--- MMFT.m
+++ MMFT.m
@@ -32,7 +32,7 @@ for i=1:options.T
     FF=zeros(m*2,2); 
     for r=idx  
         XX=cat(2,X(:,1+(r-1)*m:r*m),X(:,n+1:end));
-        K = kernel_meda(options.Kernel_type,XX,sqrt(sum(sum(XX.^0.5)/(m + m))));   
+        K = kernel_mmft(options.Kernel_type,XX,sqrt(sum(sum(XX.^0.5)/(m + m))));   
         w=ones(m,1);
         w(Ys(1+(r-1)*m:r*m)==2)=(sum(Ys(1+(r-1)*m:r*m)==1)/sum(Ys(1+(r-1)*m:r*m)==2));
         E = diag(sparse([w;zeros(m,1)])); 
@@ -66,7 +66,7 @@ end
 Yt_pred= Cls;
 end
 
-function K = kernel_meda(ker,X,sigma)
+function K = kernel_mmft(ker,X,sigma)
     switch ker
         case 'linear'
             K = X' * X;
@@ -78,46 +78,6 @@ function K = kernel_meda(ker,X,sigma)
         case 'sam'
             D = X'*X;
             K = exp(-acos(D).^2/(2*sigma^2));
-        otherwise
-            error(['Unsupported kernel ' ker])
-    end
-end
-
-
-function K = kernel_jda(ker,X,X2,gamma)
-
-    switch ker
-        case 'linear'
-
-            if isempty(X2)
-                K = X'*X;
-            else
-                K = X'*X2;
-            end
-
-        case 'rbf'
-
-            n1sq = sum(X.^2,1);
-            n1 = size(X,2);
-
-            if isempty(X2)
-                D = (ones(n1,1)*n1sq)' + ones(n1,1)*n1sq -2*X'*X;
-            else
-                n2sq = sum(X2.^2,1);
-                n2 = size(X2,2);
-                D = (ones(n2,1)*n1sq)' + ones(n1,1)*n2sq -2*X'*X2;
-            end
-            K = exp(-gamma*D); 
-
-        case 'sam'
-
-            if isempty(X2)
-                D = X'*X;
-            else
-                D = X'*X2;
-            end
-            K = exp(-gamma*acos(D).^2);
-
         otherwise
             error(['Unsupported kernel ' ker])
     end
